00d613e640fbe40cdb533bc2b083256ce0bc7c70
Hael1230 <102492080+Hael1230@users.noreply.github.com>
2023-05-29 11:36:52 -0500

    Cuarto commit
    Primer metodo get listar selecciones

---
+++ src/main/java/com/example/demoparcial2/controller/SeleccionController.java
@@ -0,0 +1,45 @@
+package com.example.demoparcial2.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demoparcial2.entitis.Seleccion;
+import com.example.demoparcial2.entitis.Continente;
+import com.example.demoparcial2.repository.SeleccionRepository;
+import com.example.demoparcial2.entitis.Resultado;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("/selecciones")
+public class SeleccionController{
+
+    @Autowired
+	SeleccionRepository seleccionRepository;
+
+    @GetMapping
+    public Continente getContinenteNombre(@PathVariable String nombre) {
+		
+		List<Seleccion> seleccion = seleccionRepository.findByNombre(nombre);
+		
+		if (!Seleccion.isEmpty()) {
+			return seleccion.get(0).getContinente();
+		}
+		
+		return null;
+
+	}
+
+
+}
+
