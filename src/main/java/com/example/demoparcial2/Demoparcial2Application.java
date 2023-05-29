177c573210c1b0a8b8b7c4c028297e8ae1efdbd9
Hael1230 <102492080+Hael1230@users.noreply.github.com>
2023-05-29 10:22:57 -0500

    primer commit

---
+++ src/main/java/com/example/demoparcial2/Demoparcial2Application.java
@@ -0,0 +1,13 @@
+package com.example.demoparcial2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoparcial2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoparcial2Application.class, args);
+	}
+
+}
